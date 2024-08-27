3f6e43354080738f243147594057e624677a1b60
Weegoos <batyr.ashim05@mail.ru>
2024-08-27 12:11:33 +0500

    Kinetic Sport System: Added Staff Page

--- src/pages/StaffPage.vue
+++ src/pages/StaffPage.vue
@@ -1,9 +1,102 @@
 <template>
-  <div>Staff</div>
+  <div class="q-pa-md">
+    <p class="text-bold text-h6">Поиск сотрудников</p>
+    <section>
+      <q-table flat bordered dense :rows="rows" :columns="columns" row-key="id">
+        <!-- Custom column for buttons -->
+        <template v-slot:body-cell-actions="props">
+          <q-td :props="props">
+            <q-btn flat size="sm" icon="edit" @click="editRow(props.row)">
+              <q-tooltip> Редактировать </q-tooltip></q-btn
+            >
+            <q-btn
+              flat
+              size="sm"
+              icon="menu"
+              @click="deleteRow(props.row)"
+              class="q-ml-sm"
+            >
+              <q-tooltip> Подробнее </q-tooltip>
+            </q-btn>
+          </q-td>
+        </template>
+      </q-table>
+    </section>
+  </div>
 </template>
 
-<script>
-export default {};
+<script setup>
+const columns = [
+  {
+    name: "id",
+    required: true,
+    label: "№",
+    field: "id",
+    align: "left",
+    sortable: true,
+  },
+  {
+    name: "fullname",
+    align: "center",
+    label: "ФИО",
+    field: "fullname",
+    sortable: true,
+  },
+  {
+    name: "physicalTraining",
+    label: "Физ. подготовка",
+    field: "physicalTraining",
+    align: "center",
+    sortable: true,
+    sort: (a, b) => parseInt(a, 10) - parseInt(b, 10),
+  },
+  {
+    name: "iin",
+    label: "ИИН",
+    field: "iin",
+    align: "center",
+    sortable: true,
+    sort: (a, b) => parseInt(a, 10) - parseInt(b, 10),
+  },
+  {
+    name: "department",
+    label: "Управление",
+    align: "center",
+    field: "department",
+    sortable: true,
+  },
+  {
+    name: "specialty",
+    label: "Специальность",
+    field: "specialty",
+    sortable: true,
+  },
+  // Add a column for actions
+  {
+    name: "actions",
+    label: "Действия",
+    align: "center",
+  },
+];
+
+const rows = [
+  {
+    id: 1,
+    fullname: "Иванов Иван Иванович",
+    iin: "123456789012",
+    department: "Управление 1",
+    specialty: "Специалист",
+    physicalTraining: "Хорошая",
+  },
+];
+
+function editRow(row) {
+  console.log("Editing row:", row);
+}
+
+function deleteRow(row) {
+  console.log("Deleting row:", row);
+}
 </script>
 
 <style></style>
