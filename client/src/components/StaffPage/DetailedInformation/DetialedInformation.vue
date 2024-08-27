2c9478521174b579bc78fdf05e2dc7bed0a0401c
Weegoos <batyr.ashim05@mail.ru>
2024-08-27 14:21:06 +0500

    Kinetic Sport System: Added tabs for in the Detailed Information page

--- client/src/components/StaffPage/DetailedInformation/DetialedInformation.vue
+++ client/src/components/StaffPage/DetailedInformation/DetialedInformation.vue
@@ -1,6 +1,38 @@
 <template>
   <div>
-    <q-dialog v-model="openDetailedInformationWindow" persistent> </q-dialog>
+    <q-dialog v-model="openDetailedInformationWindow" persistent>
+      <q-card style="width: 100vw">
+        <q-tabs
+          v-model="tab"
+          dense
+          class="bg-grey-2 text-grey-7"
+          active-color="black"
+          indicator-color="black"
+          align="justify"
+        >
+          <q-tab name="physicalTraining" label="Физ. подготовка" />
+          <q-tab name="alarms" label="Alarms" />
+          <q-tab name="movies" label="Movies" />
+          <q-btn icon="close" flat @click="closeDetailedInformationWindow" />
+        </q-tabs>
+
+        <q-tab-panels v-model="tab" animated>
+          <q-tab-panel name="physicalTraining">
+            Здесь будет физическая подготовка каждого сотрудника
+          </q-tab-panel>
+
+          <q-tab-panel name="alarms">
+            <div class="text-h6">Alarms</div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </q-tab-panel>
+
+          <q-tab-panel name="movies">
+            <div class="text-h6">Movies</div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </q-tab-panel>
+        </q-tab-panels>
+      </q-card>
+    </q-dialog>
   </div>
 </template>
 
@@ -24,7 +56,7 @@ watch(
     openDetailedInformationWindow.value = newVal;
   }
 );
-
+const tab = ref("physicalTraining");
 const emit = defineEmits(["close"]);
 const closeDetailedInformationWindow = () => {
   openDetailedInformationWindow.value = false;
