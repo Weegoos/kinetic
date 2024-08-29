5bab3aa7345e7886b110f0dd6d23a726a9b59808
Weegoos <batyr.ashim05@mail.ru>
2024-08-29 22:15:24 +0500

    Kinetic Sport System: Added Edit Dialog page

--- client/src/components/StaffPage/EditInformation/EditDialog.vue
+++ client/src/components/StaffPage/EditInformation/EditDialog.vue
@@ -2,16 +2,9 @@
   <div>
     <q-dialog v-model="confirmEditDialog" persistent>
       <q-card>
-        <q-card-section class="row items-center">
-          <q-avatar icon="signal_wifi_off" color="primary" text-color="white" />
-          <span class="q-ml-sm"
-            >You are currently not connected to any network.</span
-          >
+        <q-card-section>
+          Здесь будут поля для изменение физических данных сотрудника
         </q-card-section>
-        <q-card-actions align="right">
-          <q-btn flat label="Cancel" color="primary" @click="closeDialog" />
-          <q-btn flat label="Turn on Wifi" color="primary" v-close-popup />
-        </q-card-actions>
       </q-card>
     </q-dialog>
   </div>
